27a3f51fac2005bef296ff2639677df3ebc60c59
Svyatoslav Potrashkov <stri_69@mail.ru>
2022-11-15 16:16:12 +0300

    Added Zadacha_s_sobakoy_i_druzyami

---
+++ Zadacha_s_sobakoy_i_druzyami/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача с собакой и друзьями
+
+int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogspeed = 5;
+int friend = 2;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        int time = distance / (firstFriendSpeed + dogspeed);
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        friend = 2;
+
+    }
+    else if (friend == 2)
+    {
+        int time = distance / (secondFriendSpeed + dogspeed);
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        friend = 1;
+    }
+    count++;
+
+}
+Console.WriteLine($"Собака пробежит {count} раз");
+
